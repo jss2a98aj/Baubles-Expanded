e734e4184122b530ca6692785d87d4f46c16c3d1
jss2a98aj <jsimmons3959@neo.rr.com>
2021-12-21 01:15:03 -0500

    Added a new function to the Baubles API

--- src/main/java/baubles/api/package-info.java
+++ src/main/java/baubles/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Baubles", apiVersion = "2.0.0", provides = "Baubles|API")
+@API(owner = "Baubles", apiVersion = "2.0.1", provides = "Baubles|API")
 package baubles.api;
 
 import cpw.mods.fml.common.API;
--- src/main/java/baubles/common/ItemDebugger.java
+++ src/main/java/baubles/common/ItemDebugger.java
@@ -2,11 +2,14 @@ package baubles.common;
 
 import java.util.List;
 
+import org.lwjgl.input.Keyboard;
+
 import baubles.api.BaubleType;
 import baubles.api.expanded.BaubleExpandedSlots;
 import baubles.api.expanded.IBaubleExpanded;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
@@ -14,105 +17,116 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.StatCollector;
 
 public class ItemDebugger extends Item implements IBaubleExpanded {
 
-    public ItemDebugger() {
-       super();
-       this.setMaxStackSize(1);
-       this.setHasSubtypes(true);
-       setCreativeTab(BaublesConfig.hideDebugItem ? null : CreativeTabs.tabTools);
-    }
+	public ItemDebugger() {
+		super();
+		this.setMaxStackSize(1);
+		this.setHasSubtypes(true);
+		setCreativeTab(BaublesConfig.hideDebugItem ? null : CreativeTabs.tabTools);
+	}
 
-    @SideOnly(Side.CLIENT)
-    private IIcon[] icons;
-   
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void registerIcons(IIconRegister ir) {
-	   icons = new IIcon[BaubleExpandedSlots.getCurrentlyRegisteredTypes().size()];
-       for(int i = 0; i < icons.length; i++) {
-          icons[i] = ir.registerIcon("baubles:empty_bauble_slot_" + BaubleExpandedSlots.getCurrentlyRegisteredTypes().get(i));
-       }
-    }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public IIcon getIconFromDamage(int meta) {
-       return icons[meta >= icons.length ? 0 : meta];
-    }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void getSubItems(Item item, CreativeTabs tab, List list) {
+	@SideOnly(Side.CLIENT)
+	private IIcon[] icons;
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerIcons(IIconRegister ir) {
+		icons = new IIcon[BaubleExpandedSlots.getCurrentlyRegisteredTypes().size()];
 		for(int i = 0; i < icons.length; i++) {
-		    list.add(new ItemStack(this, 1, i));
+		  icons[i] = ir.registerIcon("baubles:empty_bauble_slot_" + BaubleExpandedSlots.getCurrentlyRegisteredTypes().get(i));
 		}
-   }
+	}
 
-   @Override
-   public String[] getBaubleTypes(ItemStack itemStack) {
-      String type;
-      int meta = itemStack.getItemDamage();
-      if(meta <= 0 || meta > icons.length) {
-         type = BaubleExpandedSlots.unknownType;
-      } else {
-         type = BaubleExpandedSlots.getCurrentlyRegisteredTypes().get(meta);
-      }
-      return new String[] {type};
-   }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public IIcon getIconFromDamage(int meta) {
+		return icons[meta >= icons.length ? 0 : meta];
+	}
 
-   @Override
-   public BaubleType getBaubleType(ItemStack itemStack) {
-      return null;
-   }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void getSubItems(Item item, CreativeTabs tab, List list) {
+		for(int i = 0; i < icons.length; i++) {
+			list.add(new ItemStack(this, 1, i));
+		}
+	}
 
-   public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) {
-      list.add("Usable bauble slot types:");
-      for(String type : getBaubleTypes(itemStack)) {
-         list.add(type);
-      }
-   }
+	@Override
+	public String[] getBaubleTypes(ItemStack itemStack) {
+	  String type;
+	  int meta = itemStack.getItemDamage();
+	  if(meta <= 0 || meta > icons.length) {
+		 type = BaubleExpandedSlots.unknownType;
+	  } else {
+		 type = BaubleExpandedSlots.getCurrentlyRegisteredTypes().get(meta);
+	  }
+	  return new String[] {type};
+	}
 
-   @Override
-   public void onWornTick(ItemStack itemStack, EntityLivingBase player) {
-   }
+	@Override
+	public BaubleType getBaubleType(ItemStack itemStack) {
+	  return null;
+	}
 
-   @Override
-   public boolean hasEffect(ItemStack itemStack, int a) {
-      return true;
-   }
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean debug) {
+		if(GuiScreen.isShiftKeyDown()) {
+			tooltip.add(StatCollector.translateToLocal("tooltip.compatibleslots"));
 
-   @Override
-   public String getUnlocalizedName(ItemStack itemStack) {
-      return super.getUnlocalizedName();// + "." + itemStack.getItemDamage();
-   }
+			String[] types = getBaubleTypes(stack);
+			for(int i = 0; i < types.length; i++) {
+				String type = StatCollector.translateToLocal("slot." + types[i]);
+				if(i < types.length - 1) type += ",";
+				tooltip.add(type);
+			}
+		} else {
+			tooltip.add(StatCollector.translateToLocal("tooltip.shiftprompt").replaceAll("%key%", Keyboard.getKeyName(42)));
+		}
+	}
 
-   @Override
-   public void onEquipped(ItemStack itemStack, EntityLivingBase player) {
-   }
+	@Override
+	public void onWornTick(ItemStack itemStack, EntityLivingBase player) {
+	}
 
-   @Override
-   public void onUnequipped(ItemStack itemStack, EntityLivingBase player) {
-   }
+	@Override
+	public boolean hasEffect(ItemStack itemStack, int a) {
+	  return true;
+	}
 
-   @Override
-   public boolean canEquip(ItemStack itemStack, EntityLivingBase player) {
-      return true;
-   }
+	/*@Override
+	public String getUnlocalizedName(ItemStack itemStack) {
+	  return super.getUnlocalizedName() + "." + itemStack.getItemDamage();
+	}*/
 
-   @Override
-   public boolean canUnequip(ItemStack itemStack, EntityLivingBase player) {
-      return true;
-   }
-   
-   @SideOnly(Side.CLIENT)
-   public IIcon getBackgroundIconForSlotType(String type) {
-       if(type != null && BaubleExpandedSlots.isTypeRegistered(type)) {
-           return icons[BaubleExpandedSlots.getIndexOfTypeInRegisteredTypes(type)];
-       } else {
-    	   return icons[0];
-       }
-   }
+	@Override
+	public void onEquipped(ItemStack itemStack, EntityLivingBase player) {
+	}
+
+	@Override
+	public void onUnequipped(ItemStack itemStack, EntityLivingBase player) {
+	}
+
+	@Override
+	public boolean canEquip(ItemStack itemStack, EntityLivingBase player) {
+	  return true;
+	}
+
+	@Override
+	public boolean canUnequip(ItemStack itemStack, EntityLivingBase player) {
+	  return true;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public IIcon getBackgroundIconForSlotType(String type) {
+		if(type != null && BaubleExpandedSlots.isTypeRegistered(type)) {
+			return icons[BaubleExpandedSlots.getIndexOfTypeInRegisteredTypes(type)];
+		} else {
+			return icons[0];
+		}
+	}
 
 }
