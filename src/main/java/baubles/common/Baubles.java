0d28465d1cb785797b5eb0989339e578063fed58
jss2a98aj <jsimmons3959@neo.rr.com>
2021-12-21 01:52:52 -0500

    Fixed internal version numbers so Thaumcraft and others mods will work

--- src/main/java/baubles/common/Baubles.java
+++ src/main/java/baubles/common/Baubles.java
@@ -28,7 +28,7 @@ public class Baubles {
 
     public static final String MODID = "Baubles";
     public static final String MODNAME = "Baubles";
-    public static final String VERSION = "GRADLETOKEN_VERSION";
+    public static final String VERSION = "2.0.3";
 
     @SidedProxy(clientSide = "baubles.client.ClientProxy", serverSide = "baubles.common.CommonProxy")
     public static CommonProxy proxy;
