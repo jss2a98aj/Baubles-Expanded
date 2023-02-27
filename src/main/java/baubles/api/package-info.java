23eae335e2a73e45ff7bc049db3764616cb9442e
jss2a98aj <jss2a98aj@gmail.com>
2023-02-27 16:34:34 -0500

    Bump API version

--- src/main/java/baubles/api/package-info.java
+++ src/main/java/baubles/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Baubles", apiVersion = "2.1.0", provides = "Baubles|API")
+@API(owner = "Baubles", apiVersion = "2.1.2", provides = "Baubles|API")
 package baubles.api;
 
 import cpw.mods.fml.common.API;
